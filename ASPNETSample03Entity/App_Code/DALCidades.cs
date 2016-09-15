3a8abb12eb2e4594f26f72d7555c1e5456a0df41
cmunhozprog02Hotmail <cmunhozprog02@hotmail.com>
2016-09-15 01:33:18 -0300

    Aula 36 - Data Acess Layer

---
+++ ASPNETSample03Entity/App_Code/DALCidades.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASPNETSample03Entity.App_Code
+{
+    public class DALCidades
+    {
+        /*Para Selecionar  */
+        public IQueryable<Cidades> Retrieve()
+        {
+            return SampleDbContext.Current.Cidades;
+        }
+
+        public void Add(Cidades c)
+        {
+
+        }
+        public void Update(Cidades c)
+        {
+
+        }
+        public void Delete(int idCidade)
+        {
+
+        }
+    }
+}
---
+++ ASPNETSample03Entity/App_Code/SampleDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASPNETSample03Entity.App_Code
+{
+    public class SampleDbContext
+    {
+        static Sample001Entities db;
+
+        public static Sample001Entities Current
+        {
+            get
+            {
+                if (System.Web.HttpContext.Current.Session["SampleDbContext"] == null)
+                {
+                    db = new Sample001Entities();
+                }
+                return db;
+            }
+        }
+    }
+}
